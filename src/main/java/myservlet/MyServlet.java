83e9aa9bbe3b15f69cf281e80d9d1325b47a3056
Oleksii Kokuiev <oleksiikokuiev@Oleksiis-MacBook-Pro.local>
2021-01-10 01:44:50 +0200

    Add new custom header Leha

--- src/main/java/myservlet/MyServlet.java
+++ src/main/java/myservlet/MyServlet.java
@@ -9,17 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 @Component
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.println(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
+        resp.addDateHeader("Leha", System.currentTimeMillis());
     }
 
     @Bean
